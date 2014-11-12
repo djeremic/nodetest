313309fdbc850240adae88c03a8f1aa61fbb9b60
Drago Jeremic <djeremic@groundlink.com>
2014-11-12 10:49:51 +0100

    adding cache

---
+++ routes/cache.js
@@ -0,0 +1,10 @@
+/**
+ * Created by drago.jeremic on 11/12/14.
+ */
+/**
+ * Created by drago.jeremic on 11/11/14.
+ */
+var NodeCache = require( "node-cache" )
+var myCache = new NodeCache();
+
+module.exports = myCache
