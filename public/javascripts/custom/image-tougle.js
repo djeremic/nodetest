6573ab1a209c480cb9a25ac6a191626811133342
Drago Jeremic <djeremic@groundlink.com>
2015-03-23 16:50:55 +0100

    UI changes

--- public/javascripts/custom/image-tougle.js
+++ public/javascripts/custom/image-tougle.js
@@ -4,6 +4,7 @@
 $(function(){
     var authorLink = $('.top a.author');
     var mapLink = $('.top a.map');
+    var headerSection = $('.top .head-wrapper');
 
     authorLink.hide();
 
@@ -11,7 +12,7 @@ $(function(){
         e.preventDefault();
         $(this).hide();
         mapLink.show();
-
+        headerSection.show();
         $('div.top').css('background-image', 'url(' + authorLink.attr('data-img') + ')');
     });
 
@@ -19,7 +20,14 @@ $(function(){
         e.preventDefault();
         $(this).hide();
         authorLink.show();
-        console.log(mapLink.attr('data-img'));
+        headerSection.hide();
         $('div.top').css('background-image', 'url(' + mapLink.attr('data-img') + ')');
+    });
+
+    var link = $('a.emerge');
+    link.animate({
+        'margin-top': '-=60px',
+        opacity: '1'
+    }, 1000, function(){
     });
 });
